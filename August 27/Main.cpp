82de1e7fef168278d5090fac9dcb2ca5d3d6f0b0
A00607597 <A00607597@3LXJ724>
2026-08-27 10:51:36 -0500

    Add project files.

---
+++ August 27/Main.cpp
@@ -0,0 +1,51 @@
+//the pink pony club
+
+#include <iostream>
+#include <string> //string of characters
+#include<Windows.h> //there is "function" that allows for a "pause" capability
+
+using namespace std;
+
+int main()
+{
+	//print the lyrics with appropriate delays
+    cout << "I ";
+	int time = 1000; //na integer is a "data type" (a type of data)
+    float PI = 3.14;
+		string songname = "Pink Pony Club";
+	
+    time = 5'000; // update variable's value to 5 seconds
+    
+    Sleep(time); //this is a 'function call"
+	cout << "love ";
+    Sleep(3'000);
+	cout << "the ";
+	
+
+
+    int main();
+
+    std::cout << "My name is Quincy Taylor"; //Quincy Taylor wrote this
+    std::cout << "My name is Roderick Walker";//Roderick Walker wrote this.
+    //std::cout << "hello, world" << std::endl;
+    std::cout << "My name is Jaleah McIntosh"; //Jaleah McIntosh wrote this
+    std::cout << "My name is Seth Norman"; //Seth Norman wrote this
+    std::cout << "My name is Taylor Prouty"; //Taylor Prouty wrote this
+    std::cout << "My name is Reuben Iria-Anenih";// Reuben Iria-Anenih wrote this
+    std::cout << "My name is Sameer Khan"; //Sameer Khan wrote this
+    std::cout << "My name is Daniel Walden"; //Daniel Walden wrote this
+    std::cout << "My name is Tim Jacques"; //Tim Jacques wrote this
+    std::cout << " my name is luis hernandez\n";// luis hernadez wrote this
+    std::cout << "My name is Jailen Jackson"; //Jailen Jackson wrote this            
+    std::cout << "My name is Quan Phan"; // Quan Phan wrote this
+    std::cout << "My name is Jerardo Rodriguez\n"; // Jerardo Rodriguez wrote this
+    std::cout << "My name is Daigo Hirata\n"; //Daigo Hirata wrote this
+    std::cout << "my name is cordaveon lewis";// cordaveon lewis wrote this 
+    std::cout << "My name is Ima"; //Imanol Rojas wrote this
+    std::cout << "My name is Juan Martinez"; //Juan Martinez wrote this. Maybe
+    std::cout << "My name is Garrett Cameron"; //Garrett wrote this.
+    std::cout << "My name is isaac demster\n"; // isaac demster wrote this.  
+    std::cout << "My name is Evan Cockrell"; // Evan Cockrell wrote this. 
+    std::cout << "my name is John Grimes"; // john grimes wrote this. bubba
+    return 0;
+}
